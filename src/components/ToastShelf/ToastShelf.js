2b690ae66c86afc9f90ed26907a98d2d8672e0bb
Jacqueline Leung <jackielineis@gmail.com>
2025-03-29 02:48:23 -0700

    Dismiss All Toasts and Accessibility Updates

--- src/components/ToastShelf/ToastShelf.js
+++ src/components/ToastShelf/ToastShelf.js
@@ -6,17 +6,12 @@ import styles from './ToastShelf.module.css';
 function ToastShelf() {
   const {toasts} = React.useContext(ToastContext);
   return (
-    <ol className={styles.wrapper}>
+    <ol className={styles.wrapper} role="region" aria-live="polite" aria-label="Notification">
       {toasts.map(({id, variant, message}) => (
         <li key={id} className={styles.toastWrapper}>
         <Toast id={id} variant={variant} >{message}</Toast>
       </li>
       ))}
-   
-      
-      {/* <li className={styles.toastWrapper}>
-        <Toast variant="error">Example error toast</Toast>
-      </li> */}
     </ol>
   );
 }
