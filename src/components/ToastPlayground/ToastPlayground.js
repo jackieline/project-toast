d6c508c43c46db23533b3bc1db41109505c22200
Jacqueline Leung <jackielineis@gmail.com>
2025-03-28 22:38:40 -0700

    Add State and Toast

--- src/components/ToastPlayground/ToastPlayground.js
+++ src/components/ToastPlayground/ToastPlayground.js
@@ -1,30 +1,59 @@
 import React from 'react';
 
 import Button from '../Button';
+import Toast from '../Toast';
 
 import styles from './ToastPlayground.module.css';
 
 const VARIANT_OPTIONS = ['notice', 'warning', 'success', 'error'];
 
 function ToastPlayground() {
+  const [message, setMessage] = React.useState('');
+  const [variant, setVariant] = React.useState(VARIANT_OPTIONS[0]);
+
+  const [showToast, setShowToast] = React.useState(false);
+
+  const handleDismiss = () => {
+    setShowToast(false);
+  }
+
+  const onChangeVariant = (event) => {
+    setVariant(event.target.value);
+  };
+
+  const onUpdateMessage = (event) => {
+    setMessage(event.target.value.trim());
+  }
+
+  const onButtonSubmit = (event) => {
+    event.preventDefault();
+    console.log(event.target);
+    console.log({message})
+    setShowToast(true);
+  }
   return (
     <div className={styles.wrapper}>
       <header>
         <img alt="Cute toast mascot" src="/toast.png" />
         <h1>Toast Playground</h1>
       </header>
+      {showToast && (<Toast handleDismiss={handleDismiss} variant={variant} >
+        {message}
+      </Toast>)
+}
+      <form onSubmit={onButtonSubmit}>
 
-      <div className={styles.controlsWrapper}>
-        <div className={styles.row}>
-          <label
-            htmlFor="message"
-            className={styles.label}
-            style={{ alignSelf: 'baseline' }}
-          >
-            Message
-          </label>
-          <div className={styles.inputWrapper}>
-            <textarea id="message" className={styles.messageInput} />
+        <div className={styles.controlsWrapper}>
+         <div className={styles.row}>
+            <label
+              htmlFor="message"
+              className={styles.label}
+              style={{ alignSelf: 'baseline' }}
+            >
+              Message
+            </label>
+            <div className={styles.inputWrapper}>
+              <textarea id="message" className={styles.messageInput} placeholder="toast content text" value={message} onChange={onUpdateMessage} />
           </div>
         </div>
 
@@ -33,17 +62,19 @@ function ToastPlayground() {
           <div
             className={`${styles.inputWrapper} ${styles.radioWrapper}`}
           >
-            <label htmlFor="variant-notice">
+            {VARIANT_OPTIONS.map(option => {
+              const uniqueId = `variant-${option}`;
+              return(<label key={uniqueId} htmlFor={uniqueId}>
               <input
-                id="variant-notice"
+                id={uniqueId}
                 type="radio"
                 name="variant"
-                value="notice"
+                value={option}
+                checked={option === variant}
+                onChange={onChangeVariant}
               />
-              notice
-            </label>
-
-            {/* TODO Other Variant radio buttons here */}
+              {option}
+            </label>)})}
           </div>
         </div>
 
@@ -51,11 +82,12 @@ function ToastPlayground() {
           <div className={styles.label} />
           <div
             className={`${styles.inputWrapper} ${styles.radioWrapper}`}
-          >
-            <Button>Pop Toast!</Button>
+           >
+              <Button>Pop Toast!</Button>
+            </div>
           </div>
         </div>
-      </div>
+      </form>
     </div>
   );
 }
